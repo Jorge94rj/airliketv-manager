38bd2d89e1696659217cd617c0e10fdc3a33d490
JorgeReyes <jorge.reyes.wm@gmail.com>
2023-07-22 05:04:21 -0600

    added-pdf

--- pages/book/[id].tsx
+++ pages/book/[id].tsx
@@ -1,32 +1,62 @@
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
 import { books } from "..";
-import { MainHeader, PillButton } from "../../styles/index.style";
+import { CloseButton, MainHeader, PdfWrapper, PillButton } from "../../styles/index.style";
 import Image from "next/image";
 import { CardContainer } from "../../components/BookCard/index.style";
+import { Document, Page } from 'react-pdf';
+import AllPages from "../../components/PdfViewer";
 
 const Book = () => {
   const router = useRouter();
   const { id } = router.query;
 
+  const [numPages, setNumPages] = useState<number>();
+  const [pageNumber, setPageNumber] = useState<number>(1);
+  const [isPdfOpened, setIsPdfOpened] = useState(false);
+
   const book = books.find(b => b.id.toString() === id);
   const alt = book?.name || "";
 
+  function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
+    setNumPages(numPages);
+  }
+
   return (
     <>
-      <MainHeader>
-        <h4>Biblioteca</h4>
-        <h2>{book?.name}</h2>
-        <h3>{book?.author}</h3>
-        <CardContainer>
-          <Image src={book?.src} alt={alt} width={380} height={480} />
-        </CardContainer>
-        <h2>Sinopsis</h2>
-        <p>{book?.details}</p>
-        <PillButton>
-          Leer
-        </PillButton>
-      </MainHeader>
+      {
+        isPdfOpened && (
+          <>
+            <PdfWrapper>
+              {/* <object data="./metamorfosis.pdf" type="application/pdf" width="100%" height="100%">
+                <p>Alternative text - include a link <a href="./metamorfosis.pdf">to the PDF!</a></p>
+              </object> */}
+              <iframe src={"/metamorfosis.pdf"} />
+              {/* <AllPages pdf="./metamorfosis.pdf" /> */}
+            </PdfWrapper>
+            <CloseButton onClick={() => setIsPdfOpened(false)}>
+              x
+            </CloseButton>
+          </>
+        )
+      }
+      {
+        !isPdfOpened && (
+          <MainHeader>
+            <h4>Biblioteca</h4>
+            <h2>{book?.name}</h2>
+            <h3>{book?.author}</h3>
+            <CardContainer>
+              <Image src={book?.src} alt={alt} width={380} height={480} />
+            </CardContainer>
+            <h2>Sinopsis</h2>
+            <p>{book?.details}</p>
+            <PillButton onClick={() => setIsPdfOpened(true)}>
+              Leer
+            </PillButton>
+          </MainHeader>
+        )
+      }
     </>
   )
 }
--- styles/index.style.tsx
+++ styles/index.style.tsx
@@ -31,3 +31,30 @@ export const PillButton = styled.button`
   padding: 16px;
   margin: 24px 0;
 `;
+
+export const CloseButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  width: 32px;
+  height: 32px;
+  position: absolute;
+  top: 16px;
+  left: 16px;
+  z-index: 99;
+  border-radius: 50%;
+`;
+
+export const PdfWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 48;
+  width: 100vw;
+  height: 100vh;
+  iframe {
+    width: 100%;
+    height: 100%;
+  }
+`;
